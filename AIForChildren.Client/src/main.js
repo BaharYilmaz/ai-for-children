c7c3a5863e512a7bf77883b250b427f96b3d24f3
berkansasmaz <berkansasmazz@gmail.com>
2020-04-02 13:07:11 +0300

    Bootstrap-Vue kütüphanesi eklendi.

--- AIForChildren.Client/src/main.js
+++ AIForChildren.Client/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import VueRouter from "vue-router"
 
+//Router
+import VueRouter from "vue-router"
 Vue.use(VueRouter);
 
+//Components
 import App from './App.vue' 
 import Home from "./components/Home.vue"
 import Register from "./components/Register.vue"
@@ -10,6 +12,15 @@ import Login from "./components/Login.vue"
 
 Vue.config.productionTip = false
 
+//Module
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+
+//Router
 const router =new VueRouter({
   routes:[
     {path: "/", component: Home },
@@ -18,7 +29,8 @@ const router =new VueRouter({
   ],
   mode:"history"
 })
-
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 new Vue({
   render: h => h(App),
   router
