53b7eb16f2a441e5721b4a02bd21efb47ece5a2b
Zisan Karsatar <zisankarsatar@gmail.com>
2020-04-08 15:15:28 +0300

    Navbar added

--- AIForChildren.Client/src/App.vue
+++ AIForChildren.Client/src/App.vue
@@ -1,16 +1,22 @@
 <template>
-    <div class="id">
+    <div class="App">
       <navbar></navbar>
       <router-view></router-view>   
+      <Footer></Footer>
     </div>
+     
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue"
+import Footer from "./components/Footer.vue"
 
 export default {
   name: 'App',
-  components: {Navbar}
+  components:{ 
+  Navbar ,
+  Footer,
+  }
 }
 </script>
 
