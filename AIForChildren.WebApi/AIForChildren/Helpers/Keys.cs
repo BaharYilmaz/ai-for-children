010db82607f7ab4fc24029a7b544b5d350fc7800
berkansasmaz <berkansasmazz@gmail.com>
2020-04-09 20:15:46 +0300

    The keys required for the configuration were gathered somewhere.

---
+++ AIForChildren.WebApi/AIForChildren/Helpers/Keys.cs
@@ -0,0 +1,9 @@
+namespace MusteriPaneli.WebApi.Helpers
+{
+    public class Keys
+    {
+        public const string ConnectionString = "AIFORCHILDREN_CONNECTIONSTRING";
+        public const string TestProject = "AIFORCHILDREN_TEST_PROJECT";
+
+    }
+}
