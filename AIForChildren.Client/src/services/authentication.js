eec6a4ef9f6daf845c0a9395819671a4f3062ba7
berkansasmaz <berkansasmazz@gmail.com>
2020-04-10 02:54:28 +0300

    Service created for Authentication.

---
+++ AIForChildren.Client/src/services/authentication.js
@@ -0,0 +1,17 @@
+import {
+  http
+} from "../utils/http";
+
+const AuthenticationService = {
+  async save(value) {
+    var result = await http.post("https://localhost:5001/api/auth/register", value);
+    if (result.status === 200) {
+      return result.data;
+    } else {
+      console.error(result.error);
+      throw result.error;
+    }
+  }
+};
+
+export default AuthenticationService;
